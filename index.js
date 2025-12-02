d633bff41cd382d4059243f512535fb9d57ce0fe
Danae Barba <danaebarba14@gmail.com>
2025-12-02 15:50:55 -0600

    Test deploy

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const PORT = process.env.PORT || 3000;
 const VERSION = process.env.APP_VERSION || 'v1';
 
 app.get('/', (req, res) => {
-  res.send(`App running – ${VERSION}`);
+  res.send("Nueva versión desplegada automáticamente 🎉");
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}, version ${VERSION}`);
 });
-console.log("Deploy actualizado correctamente!");
